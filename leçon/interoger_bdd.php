bbda300d189acad031bd0c02a7f18fde90815b63
Johnny <139769556+Sjossa@users.noreply.github.com>
2023-11-03 10:11:49 +0100

    Update interoger_bdd.php

--- leçon/interoger_bdd.php
+++ leçon/interoger_bdd.php
@@ -5,15 +5,47 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <?php
-//interroger une basse de donnée a l'aide de l'API Mysqli
-    /*
-    on interroge les informations d'une table (bdd) par l'intermediaire d'une requete SQL qui renvoie un je ude resultat qu'on affichje par la suite dans le navigateur (ouclient).
-    */
+<?php
+$cnx = mysqli_connect("localhost", "root", "", "casse2");
 
-    ^cnx = mysqli_connect("localhost","root","casse2")
-    if(mysqli_connect)
+if (mysqli_connect_errno()) {
+    echo "Erreur de connexion à la base " . mysqli_connect_error();
+}
+
+
+$sql = "SELECT * FROM personne ORDER BY numach ASC";
+
+if ($result = mysqli_query($cnx, $sql)) {
+    echo "Pour cette requête <br> " . $sql . "<br><br>";
+    echo "Il y a " . mysqli_num_rows($result) . " réponses <br><br>";
+}
+
+echo "<table class='table table-hover'>";
+echo "<th>" . "NumAch" . "</th>";
+echo "<th>" . "Nom" . "</th>";
+echo "<th>" . "Age" . "</th>";
+echo "<th>" . "Ville" . "</th>";
+echo "<th>" . "Sexe" . "</th>";
+
+// Pour l'ensemble des lignes du jeu de résultats, répète while($row = mysqli_fetch_row($result)) {
+while ($row = mysqli_fetch_row($result)) {
+    echo "<tr>";
+    echo "<td>" . $row[0] . "</td>";
+    echo "<td>" . $row[1] . "</td>";
+    echo "<td>" . $row[2] . "</td>";
+    echo "<td>" . $row[3] . "</td>";
+    echo "<td>" . $row[4] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+
+// Annule le jeu de résultats
+mysqli_free_result($result);
+
+// Ferme la connexion à la base de données
+mysqli_close($cnx);
+?>
 </body>
 </html>
