c1d5d9c0b064158b02cfb84ad1bdfea77effb633
Sjossa <johnny.sass2001@gmail.com>
2023-11-08 16:28:21 +0100

    leçon

--- leçon/insert-donné.php
+++ leçon/insert-donné.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,26 +7,30 @@
 </head>
 <body>
     <?php
-
-    /*
-    Insere les données dans Mysql
-
-    Pour insere de nouveau enrengistrement dans une table , on se sert de la commande SQL insert into
-    les chaines sont placées entre apostrophe, execpté pour les valeurs numérique ou la valeur NULL, s'il (nt a pas de donnné a insérer.)
-    Il y autant de valeurs que de champs
-    */
-
-    $con = mysqli_connect("localhost","root", "casse2");
-    if (mysqli_connect_errno()){
-        echo " erreur". mysqli_connect_errno();
-        exit()
+    // Connexion à la base de données MySQL
+    $con = mysqli_connect("localhost", "root", "", "casse2");
+    if (mysqli_connect_errno()) {
+        echo "Erreur : " . mysqli_connect_error();
+        exit(); // Quitte le script en cas d'erreur de connexion
     }
-// si cela marche on crée la requete sql
 
-$sql  "insert into personne (NumAch, Nom, Age, Ville, Sexe) values('','alain','32','manchester','M')";
-echo " insertion réelaiser";
-$result = mysqli_query($con,$sql) or die ( " echec");
-// fermer la connexion
-mysqli_close($con);
+    // Crée une requête SQL pour insérer des données dans la table 'personne'
+    $sql = "INSERT INTO personne (NumAch, Nom, Age, Ville, Sexe) VALUES ('', 'alain', '32', 'manchester', 'M')";
+
+    // Exécute la requête SQL
+    $result = mysqli_query($con, $sql);
+
+    if ($result) {
+        echo "Insertion réussie"; 
+    } else {
+        echo "Échec de l'insertion : " . mysqli_error($con); 
+    }
+    echo "<br>";
+    echo $sql; // Ajout d'un point-virgule et de la balise de fermeture PHP
+
+    // Ferme la connexion à la base de données
+    mysqli_close($con);
+    ?>
 </body>
 </html>
+
