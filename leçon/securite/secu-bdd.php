2b5526b7a1ba7cbb86d1be3d2f3fa3286db0b787
Sjossa <johnny.sass2001@gmail.com>
2023-12-04 16:31:03 +0100

    la

---
+++ leçon/securite/secu-bdd.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    // Vérification des champs obligatoires
+    if (!isset($_POST["nom"]) || empty($_POST["nom"])) {
+        echo "Le nom est obligatoire";
+        exit();
+    }
+
+    if (!isset($_POST["prenom"]) || empty($_POST["prenom"])) {
+        echo "Le prénom est obligatoire";
+        exit();
+    }
+
+    if (!isset($_POST["email"]) || empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+        echo "L'adresse email est obligatoire et doit être valide";
+        exit();
+    }
+
+    if (!isset($_POST["mdp"]) || empty($_POST["mdp"])) {
+        echo "Le mot de passe est obligatoire";
+        exit();
+    }
+
+    // Échappement des données
+    $nom = htmlspecialchars($_POST["nom"]);
+    $prenom = htmlspecialchars($_POST["prenom"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $mdp = htmlspecialchars($_POST["mdp"]);
+
+    // Connexion à la base de données
+    $dsn = "mysql:host=localhost;dbname=secu_bdd";
+    $user = "root";
+    $pwd = "";
+
+    try {
+        $pdo = new PDO($dsn, $user, $pwd);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Erreur de connexion à la base de données" . $e->getMessage();
+        exit();
+    }
+
+//insertion des données dans la base des données
+  $sql = "INSERT INTO utilisateurs (nom, prenom, mdp, mail) VALUES (?, ?, ?, ?)";
+  $stmt = $pdo->prepare($sql);
+$stmt->execute([$nom,$prenom,$mdp,$email ]) ;
+
+echo "Vos données sont enregistrées";
+?>
+</body>
+</html>
